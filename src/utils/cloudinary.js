3ab442502160d4530b47d90145bd9e0b5a9dafc0
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2025-04-12 14:03:40 +0530

    Add Cloudinary utility for file uploads with error handling

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import logger from "./logger.js";
+
+// Configuration Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // Upload file on Cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    logger.info(`File uploaded successfully. File path: ${response.url}`);
+
+    // File has been uploaded successfully and unlinked from the cloudinary after the upload completes
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    // Remove the locally save temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
