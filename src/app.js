d08a70b0b92bd70380e7e8f44170371013b011e0
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2025-06-09 11:37:46 +0530

    add express app setup with middleware for CORS, JSON parsing, static files, and logging

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { logger } from "./utils/logger.js";
+
+const app = express();
+
+// Format of the logger
+const morganFormat = ":method :url :status :response-time ms";
+
+// Common Middlewares
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// Logger Middleware
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+export { app };
