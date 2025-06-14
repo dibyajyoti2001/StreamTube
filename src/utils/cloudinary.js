8346cb1b6abe8e233f74d6766318ed5c2bb162b2
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2025-06-14 21:01:49 +0530

    fix: update import statements to use .js extension for consistency

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import logger from "./logger.js";
+import { logger } from "./logger.js";
 
 // Configuration Cloudinary
 cloudinary.config({
