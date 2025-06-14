7abe8797da85e03ee8008d778b8a5a65afff93fa
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2025-06-14 15:52:58 +0530

    add login route to user routes for handling user authentication

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -17,5 +17,6 @@ router.route("/register").post(
   ]),
   registerUser
 );
+router.route("/login").get(loginUser);
 
 export default router;
